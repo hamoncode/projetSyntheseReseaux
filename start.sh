463b760ea12aa521035f84a17e731f03d3eef841
hamoncode <vincent.hamon23@gmail.com>
2025-04-18 19:47:22 -0400

    droit sudo obligatoire pour script

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# verifier que le script est exécuté avec les droits root
+if [ "$EUID" -ne 0 ]; then
+    echo "Veuillez exécuter ce script avec sudo."
+    exit 1
+fi
+
 set -e
 
 # Active l'environnement virtuel
@@ -13,7 +19,7 @@ FW_PID=$!
 
 # lancer tcpdump pour surveiller le trafic réseau
 echo "Lancement de tcpdump pour surveiller le trafic réseau..."
-sudo tcpdump -i any -n -s 0 -w test.pcap &
+tcpdump -i any -n -s 0 -w test.pcap &
 
 # Attendre que tcpdump démarre
 sleep 2
@@ -23,7 +29,7 @@ sleep 2
 
 # Ajouter la règle qui redirige le trafic vers le pare-feu
 echo "Ajout de la règle iptables..."
-sudo iptables -I INPUT -j NFQUEUE --queue-num 1
+iptables -I INPUT -j NFQUEUE --queue-num 1
 
 echo "Pare-feu actif (PID: $FW_PID)."
 echo "Pour arrêter : 'sudo iptables -D INPUT -j NFQUEUE --queue-num 1' puis tuer le processus $FW_PID"
