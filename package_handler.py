a8cf593d7295ddadee3c058776db9946176dea7f
hamoncode <vincent.hamon23@gmail.com>
2025-04-18 16:51:55 -0400

    test basic firewall

--- package_handler.py
+++ package_handler.py
@@ -2,24 +2,12 @@ import json
 from scapy.all import IP, TCP, UDP
 from logger import setup_logger
 
-with open("config.json") as f:
-    config = json.load(f)
-
-logger = setup_logger()
-
 def is_suspicious(packet):
     ip_layer = packet.getlayer(IP)
 
-    if ip_layer.src in config["blocked_ips"]:
-        logger.info(f"Blocked IP: {ip_layer.src}")
-        return True
-
-    if packet.haslayer(TCP) and packet[TCP].dport in config["blocked_ports"]:
-        logger.info(f"Blocked TCP port {packet[TCP].dport} from {ip_layer.src}")
-        return True
-
-    if packet.haslayer(UDP) and packet[UDP].dport in config["blocked_ports"]:
-        logger.info(f"Blocked UDP port {packet[UDP].dport} from {ip_layer.src}")
+    if packet.haslayer(TCP) and packet[TCP].dport == 12345:
+        logger.info(f"Blocked TCP port 12345 from {ip_layer.src}")
         return True
 
     return False
+
