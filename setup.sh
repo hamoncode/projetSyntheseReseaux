721856d12e80d5282abe8185d599edb605953392
hamoncode <vincent.hamon23@gmail.com>
2025-04-18 20:27:55 -0400

    run as a service

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# verifier que le script est exécuté avec les droits root
+if [ "$EUID" -ne 0 ]; then
+    echo "Veuillez exécuter ce script avec sudo."
+    exit 1;
+fi
+
 set -e
 
 echo "[*] Mise à jour des paquets..."
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 echo "[*] Installation des dépendances système pour le pare-feu Python..."
-sudo apt install -y \
+apt install -y \
     python3 \
     python3-pip \
     python3-venv \
@@ -17,16 +23,40 @@ sudo apt install -y \
     iptables \
     tcpdump
 
-echo "Création d'un environnement virtuel Python (venv)..."
+echo "[*] Création d'un environnement virtuel Python (venv)..."
 python3 -m venv firewall
 
-echo "Activation de l'environnement virtuel..."
+echo "[*] Activation de l'environnement virtuel..."
 source firewall/bin/activate
 
-echo "Mise à jour de pip..."
+echo "[*] Mise à jour de pip..."
 pip install --upgrade pip
 
-echo "Installation des dépendances Python..."
+echo "[*] Installation des dépendances Python..."
 pip install scapy netfilterqueue
 
-echo "installation des dépendances reussi"
+echo "[*] Création et activation du service systemd..."
+
+SERVICE_PATH="/etc/systemd/system/firewall.service"
+PROJECT_PATH="$(pwd)/firewall.service"
+
+if [ ! -f "$PROJECT_PATH" ]; then
+    echo "[!] Le fichier firewall.service est introuvable dans le projet."
+    exit 1
+fi
+
+echo "→ Lien symbolique vers systemd..."
+ln -sf "$PROJECT_PATH" "$SERVICE_PATH"
+
+echo "→ Rechargement du démon systemd..."
+systemctl daemon-reexec
+systemctl daemon-reload
+
+echo "→ Activation du service firewall..."
+systemctl enable firewall
+
+echo "→ Démarrage du service firewall..."
+systemctl start firewall
+
+echo "→ Statut du pare-feu :"
+systemctl status firewall --no-pager
