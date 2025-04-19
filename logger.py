4b886bc8c740fc0ba52f62bac9ec4039c070f74e
hamoncode <vincent.hamon23@gmail.com>
2025-04-18 22:20:56 -0400

    test absolute path

--- logger.py
+++ logger.py
@@ -2,12 +2,14 @@ import os
 import logging
 
 def setup_logger(log_file=None):
+    # Chemin absolu garanti pour le log
     if log_file is None:
-        log_file = os.path.join(os.getcwd(), "firewall.log")
+        log_file = "/home/firewalltester/projetSyntheseReseaux/firewall.log"
 
     logging.basicConfig(
         filename=log_file,
         level=logging.INFO,
         format='%(asctime)s %(levelname)s: %(message)s'
     )
+
     return logging.getLogger("firewall")
